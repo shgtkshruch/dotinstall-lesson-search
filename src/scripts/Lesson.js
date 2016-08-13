d09b20946b620ea523459464f296fa6284f6b4ca
shgtkshruch <shgtk.shruch@gmail.com>
2016-08-13 17:58:42 +0900

    Performance

--- src/scripts/Lesson.js
+++ src/scripts/Lesson.js
@@ -1,14 +1,20 @@
 const Lesson = React.createClass({
+  shouldComponentUpdate(nextProps, nextState) {
+    return false;
+  },
+
   render() {
+    const contents = this.props.lesson.contents.map((content, index) => {
+      return <Content key={index} content={content} />
+    });
+
     return (
       <li className="lesson__item">
-        <a href={this.props.link}>
+        <a href={this.props.lesson.link}>
           {this.props.lesson.name}
         </a>
         <ul className="lesson__content">
-          {this.props.lesson.contents.map((content, index) => {
-            return <Content key={index} content={content} />
-          })}
+          {contents}
         </ul>
       </li>
     )
